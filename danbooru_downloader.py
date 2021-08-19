4236d54f07b77a68aa9bb2f15298f8b2a47bdc40
strd3983 <strd3983@gmail.com>
2021-08-19 18:03:37 +0900

    v1.1 - マイナー修正

--- danbooru_downloader.py
+++ danbooru_downloader.py
@@ -5,8 +5,6 @@ import urllib
 import configparser
 from bs4 import BeautifulSoup as bs4
 
-# import pandas as pd
-
 
 # --------------------------------------------------
 # メイン関数
@@ -18,12 +16,15 @@ def main():
         return
     for url in urls:
         print('\n探索:', url)
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-        page = requests.get(url, headers=headers)
+        page = requests.get(url)
         soup = bs4(page.text, 'html5lib')
-        index = url.split('/')[-1]  # danbooruのidをファイル名に
-        series = get_tag(soup, 'copyright-tag-list')  # 原作品名またはオリジナル
-        artist = get_tag(soup, 'artist-tag-list')  # 絵師名
+
+        # メタデータを取得
+        # index:id, series:原作品名またはオリジナル, artsit:絵師名
+        index = url.split('/')[-1]
+        series = get_tag(soup, 'copyright-tag-list')
+        artist = get_tag(soup, 'artist-tag-list')
+
         # メタデータに基づきディレクトリの作成
         try:  # 版権モノの場合
             charas = get_tag(soup, 'character-tag-list')  # キャラ名
@@ -35,30 +36,34 @@ def main():
 
         # 画像データの取得
         link = soup.find('img', {'class': 'fit-width'})
-        src = link.get('src').replace('sample-',
-                                      '').replace('sample', 'original')
+        src = link.get('src').replace('sample-', '')
+        src = src.replace('sample', 'original')  # 元データを参照
         save_img(src, os.path.join(img_dir, index))
         print(img_dir, 'に保存しました')
 
+        # 元画像へのリンク
+        pixiv = soup.find('a', {
+            'rel': 'external noreferrer nofollow'
+        }).get_text()
+        print(artist, '|', 'https://www.' + pixiv)
+        print('-----------------------------------------------')
+
 
 # --------------------------------------------------
-# 設定iniファイルの読み込み
+# .URLの読み込み
 # --------------------------------------------------
 def config():
     config_ini = configparser.ConfigParser(interpolation=None)
-    i = 0
-    urls = []
+    urls = []  # URLの集合
     for config_path in glob.glob('./URL/*.URL'):
-        i += 1
         # iniファイルが存在する場合、ファイルを読み込む
         with open(config_path, encoding='utf-8') as fp:
             config_ini.read_file(fp)
-            # iniの値取得
             url = config_ini['InternetShortcut']['URL']
             url = url.split('?')
             urls.append(url[0])
-        os.remove(config_path)
-    # 設定出力
+        f_path = os.path.join('./URL', url[0].split('/')[-1] + '.txt')
+        os.rename(config_path, f_path)
     return urls
 
 
@@ -77,12 +82,12 @@ def get_tag(soup, find_cls):
 # 画像の保存
 # --------------------------------------------------
 def save_img(url, dir_path):
-    try:  # jpgの場合
+    try:  # .jpgの場合
         with urllib.request.urlopen(url) as web_file:
             data = web_file.read()
             with open(dir_path + '.jpg', mode='wb') as local_file:
                 local_file.write(data)
-    except urllib.error.URLError:  # pngの場合
+    except urllib.error.URLError:  # .pngの場合
         with urllib.request.urlopen(url.replace('.jpg', '.png')) as web_file:
             data = web_file.read()
             with open(dir_path + '.png', mode='wb') as local_file:
@@ -93,5 +98,5 @@ def save_img(url, dir_path):
 
 if __name__ == "__main__":
     main()
-    # print('終了しました')
-    # os.system('PAUSE')
+    print('終了しました')
+    os.system('PAUSE')
